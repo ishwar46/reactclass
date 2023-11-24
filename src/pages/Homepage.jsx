315fbb6bbb8c1d6c8e47f0d5e533a88605b26051
Ishwar Chaudhary <ishwarura@gmail.com>
2023-11-24 09:06:31 +0545

    update

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,13 @@
+// importing
+import React from "react";
+// function
+const Homepage = () => {
+    return (
+        <div>
+            <h1>HomePage from page folder</h1>
+        </div>
+    )
+}
+// export default
+
+export default Homepage;
